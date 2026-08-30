bdf4f866e0be106372ecf1df521b1e2b0c8d0b07
savvygonsama <317450077+savvygonsama@users.noreply.github.com>
2026-08-30 14:35:14 +0900

    스틸링고 Lite를 최신본(20회차)으로 교체
    배포본이 5회차짜리 구버전이었다. 아티팩트에 있던 최신본으로 갈아끼우고
    PWA 설치 기능을 다시 입혔다. 서비스워커 캐시는 v2로 올려 기존 설치분도
    새 내용을 받도록 했다.

--- steel-lingo/lite/sw.js
+++ steel-lingo/lite/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'steel-lingo-lite-v1';
+const CACHE = 'steel-lingo-lite-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
